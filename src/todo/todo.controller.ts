1498cde1007a130dbee96e9ddd9e90dfebbd64b9
nacukisubaru <alexitsheff@gmail.com>
2022-06-29 23:48:54 +0300

    onCommandCreate вывод агрументов которые передали боту в сообщении

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { OnCommand } from 'discord-nestjs';
+import { Content, Context, OnCommand } from 'discord-nestjs';
 import { Message } from 'discord.js';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoService } from './todo.service';
 
-@Controller('todo')
+@Controller()
 export class TodoController {
     
     constructor(private todoService: TodoService) {}
 
-    @Post('/create')
-    createTodo(@Body() todoDto: CreateTodoDto) {
-       return this.todoService.add(todoDto);
+    // @Post('/create')
+    // createTodo(@Body() todoDto: CreateTodoDto) {
+    //    return this.todoService.add(todoDto);
+    // }
+
+    @OnCommand({ name: 'createTodo' })
+    async onCommandCreate( @Content() content: CreateTodoDto,
+    @Context() [context]: [Message],): Promise<void> {
+        await context.reply(`Execute command: ${content}`);
     }
 }
